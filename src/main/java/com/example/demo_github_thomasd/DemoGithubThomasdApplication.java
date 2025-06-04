131b4b6ccda4f9e14b9e83df9c24cf0400ca56e5
Grimyolf <95622304+Grimyolf@users.noreply.github.com>
2025-06-04 09:30:01 +0200

    first commit

---
+++ src/main/java/com/example/demo_github_thomasd/DemoGithubThomasdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_github_thomasd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubThomasdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGithubThomasdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_github_thomasd/DemoGithubThomasdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_github_thomasd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubThomasdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
